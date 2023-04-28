a72cf78e93aa641df77c0900901ac7e84c30a455
pusar <pusar@LAPTOP-CG6GQTSI>
2023-04-28 11:32:35 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using Services;
+
+
+namespace DependencyInjectionExample.Controllers
+{
+    public class HomeController : Controller
+    {
+
+       private readonly CitiesService _citiesService;
+        public HomeController()
+        {
+            _citiesService = new CitiesService();
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+              List<string> cities =  _citiesService.GetCities().ToList();
+            //  return View(cities);
+           // citiesService = null;
+          return View(cities);
+        }
+    }
+}
